d3a1595e8e873448640f71a9fd6565a5ef0c9cb1
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-09-21 18:23:10 -0300

    feat: compile ts

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,5 +1,10 @@
 //modelagem foi feita utilizando classes
 export class Negociacao {
+
+    private _data;
+    private _quantidade;
+    private _valor;
+
     //construtor é um metodo especial para criar e inicializar um objeto criado a partir de uma classe
     // sintaxe: constructor([argumentos]){...}
     //o this esta sendo utilizado para se referenciar a classe "pai"
@@ -8,17 +13,22 @@ export class Negociacao {
         this._quantidade = quantidade;
         this._valor = valor;
     }
+
     //o get consegue acessar os atributos privados
     get data() {
         return this._data;
     }
+
     get quantidade() {
         return this._quantidade;
     }
+
     get valor() {
         return this._valor;
     }
+
     get volume() {
         return this._quantidade * this._valor;
     }
+
 }
