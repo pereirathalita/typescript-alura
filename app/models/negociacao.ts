d41afd1620eb3dd0f870da6ffbca2659611e44f4
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-09-24 15:13:30 -0300

    feat: tipagem

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,33 +1,32 @@
 //modelagem foi feita utilizando classes
 export class Negociacao {
-
-    private _data;
-    private _quantidade;
-    private _valor;
+    private _data: Date;
+    private _quantidade: number;
+    private _valor: number;
 
     //construtor é um metodo especial para criar e inicializar um objeto criado a partir de uma classe
     // sintaxe: constructor([argumentos]){...}
     //o this esta sendo utilizado para se referenciar a classe "pai"
-    constructor(data, quantidade, valor) {
+    constructor(data: Date, quantidade: number, valor: number) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
 
     //o get consegue acessar os atributos privados
-    get data() {
+    get data(): Date {
         return this._data;
     }
 
-    get quantidade() {
+    get quantidade(): number {
         return this._quantidade;
     }
 
-    get valor() {
+    get valor(): number {
         return this._valor;
     }
 
-    get volume() {
+    get volume(): number {
         return this._quantidade * this._valor;
     }
 
