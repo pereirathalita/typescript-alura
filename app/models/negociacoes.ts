f2188f5e4afb1dfa1bba73a5efcedc5839f59862
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-10-01 14:40:53 -0300

    feat: centralizando updates

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -4,11 +4,11 @@ export class Negociacoes {
     //Negociacao[] é a mesma coisa que dizer Array<Negociacao>
     private negociacoes: Negociacao[] = [];
 
-    adiciona(negociacao: Negociacao) {
+    public adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
     //readonly Negociacao[] é a mesma coisa que dizer ReadonlyArray<Negociacao>
-    lista(): readonly Negociacao[] {
+    public lista(): readonly Negociacao[] {
         return this.negociacoes;
     }
 }
