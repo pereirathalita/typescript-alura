c9d721512fd71a64cd0429f2e96eb83306be0aa6
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-10-02 18:51:44 -0300

    feat: atualização

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,20 +1,21 @@
-//modelagem foi feita utilizando classes
 export class Negociacao {
-    //construtor é um metodo especial para criar e inicializar um objeto criado a partir de uma classe
-    // sintaxe: constructor([argumentos]){...}
-    //o this esta sendo utilizado para se referenciar a classe "pai"
     constructor(_data, quantidade, valor) {
         this._data = _data;
         this.quantidade = quantidade;
         this.valor = valor;
     }
-    //o get consegue acessar os atributos privados
-    //o get não pode ter o mesmo nome da propriedade (propriedade = o que o constructor recebe)
     get volume() {
         return this.quantidade * this.valor;
     }
     get data() {
         const data = new Date(this._data.getTime());
         return data;
+    }
+    static criaDe(dataString, quantidadeString, valorString) {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
     }
 }
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,12 +1,10 @@
 export class Negociacoes {
     constructor() {
-        //Negociacao[] é a mesma coisa que dizer Array<Negociacao>
         this.negociacoes = [];
     }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
-    //readonly Negociacao[] é a mesma coisa que dizer ReadonlyArray<Negociacao>
     lista() {
         return this.negociacoes;
     }
